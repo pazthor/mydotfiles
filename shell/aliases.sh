93359c394152bbf90812e4f9608b322b5e3632e5
Julio Pastor <julio.pastor@creditas.com>
2021-10-05 18:25:46 -0500

    add new aliase: gradlew

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -6,25 +6,30 @@ alias ...="cd ../.."
 alias ll="ls -l"
 alias la="ls -la"
 alias ~="cd ~"
-alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
 alias gaa="git add -A"
-alias gc='$DOTLY_PATH/bin/dot git commit'
+alias gc="dot git commit"
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
-alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
+alias gd="dot git pretty-diff"
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gl="dot git pretty-log"
 
 # Utils
 alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
-alias up='dot package update_all'
+
+
+#work
+alias gdwall='./gradlew clean && ./gradlew build && ./gradlew detekt'
+alias gdwclean='./gradlew clean'
+alias gdwbld='./gradlew build'
+alias gdw='./gradlew detekt'
